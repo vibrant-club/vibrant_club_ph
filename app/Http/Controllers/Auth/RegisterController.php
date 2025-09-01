6a4560e994bce1f09d222f821d74addaed44e7ab
cespena <cespena@dbm.gov.ph>
2025-09-01 13:38:00 +0800

    3 days

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -132,7 +132,7 @@ class RegisterController extends Controller
         // Determine expiration based on sub_plan
         switch ($code->sub_plan) {
             case 0:       // trial plan (1 day)
-                $expiredAt = Carbon::now()->addDay();
+                $expiredAt = Carbon::now()->addDays(3);
                 break;
             case 1:       // 1 month plan
                 $expiredAt = Carbon::now()->addMonth();
